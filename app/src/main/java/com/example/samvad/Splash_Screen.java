9a94ada38b2766a32cbfa02c9a75b94bc05d9f45
Abhishek Kumar Shaw <avitech2001@gmail.com>
2023-01-06 01:42:18 +0530

    Animation work along with lottie, gif added and splashscreen ready

---
+++ app/src/main/java/com/example/samvad/Splash_Screen.java
@@ -0,0 +1,55 @@
+package com.example.samvad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class Splash_Screen extends AppCompatActivity {
+    /*TextView AppName;*/
+    LottieAnimationView lottie;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        /*AppName = findViewById(R.id.AppName);*/
+        lottie = findViewById(R.id.lottie);
+
+
+        lottie.animate().setDuration(3500).setStartDelay(20);
+        lottie.animate().getInterpolator();
+        lottie.playAnimation();
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+
+                Intent i = new Intent(Splash_Screen.this,MainActivity.class);
+                startActivity(i);
+                finish();
+
+                /*if(mAuth.getCurrentUser()!=null){
+                    Intent i = new Intent(splashScreen.this,MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+                else{
+                    Intent i = new Intent(splashScreen.this,login_page.class);
+                    startActivity(i);
+                    finish();
+                }*/
+
+            }
+        },5000);
+    }
+}
